76f42c6e11b60dccdfa8e8da98b40d26a0ecf879
Karolina Kowalczyk <224340@edu.p.lodz.pl>
2021-03-28 21:44:54 +0200

    Add html and css for the main page

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+
+
+    constructor() {
+        setInterval(this.showNavbarOnScroll, 1000);
+    }
+
+    showNavbarOnScroll(): void
+    {
+        console.log('Cokolwiek');
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+            // @ts-ignore
+            document.getElementById('navigation').style.top = '0';
+        }
+        else {
+            // @ts-ignore
+            document.getElementById('navigation').style.top = '-5vw';
+        }
+    }
 }
+
+
+
