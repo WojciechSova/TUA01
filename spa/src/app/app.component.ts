d21d74b23d29bd46cdaa04f1a67da344a5f6f472
Karolina Kowalczyk <224340@edu.p.lodz.pl>
2021-03-29 19:39:42 +0200

    Fix sliding navbar

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements AfterViewInit{
 
 
     @ViewChild('logo')
@@ -13,33 +13,28 @@ export class AppComponent implements OnInit{
     @ViewChild('navigation')
     private navigation: ElementRef | undefined;
 
-    private obsOptions: {
-        root: null;
-        threshold: 0;
-    } | undefined;
 
-    ngOnInit(): void {
-        console.log(this.logo);
-        const observer = new IntersectionObserver(this.showNavbarOnScroll, this.obsOptions);
-        // @ts-ignore
-        observer.observe(document.getElementById('logo'));
+    ngAfterViewInit(): void {
+        const obsOptions: any = {
+            root: null,
+            threshold: 0
+        };
+
+        const observer = new IntersectionObserver(this.showNavbarOnScroll.bind(this), obsOptions);
+        observer.observe(this.logo?.nativeElement);
     }
 
-
     showNavbarOnScroll(entries: any): void {
-        console.log('Cokolwiek');
-        if (document.documentElement.scrollTop > 20) {
-            // @ts-ignore
-            document.getElementById('navigation').style.top = '0';
-            console.log('if');
-        }
-        if (!entries.isIntersecting) {
-            // @ts-ignore
-            document.getElementById('navigation').style.top = '-5vw';
-            console.log('else');
+        if (!entries[0].isIntersecting) {
+            this.navigation?.nativeElement.classList.add('nav-sticky');
+            this.navigation?.nativeElement.classList.remove('nav-non-sticky');
+        } else {
+            this.navigation?.nativeElement.classList.add('nav-non-sticky');
+            this.navigation?.nativeElement.classList.remove('nav-sticky');
         }
     }
 }
+
 
 
 
