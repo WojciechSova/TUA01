5bbed1ce4783f8dfa2210d05b9411c324ce3ecdd
Karolina Kowalczyk <224340@edu.p.lodz.pl>
2021-03-29 15:14:46 +0200

    Add sliding down navigation bar

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -1,27 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
 
-    constructor() {
-        setInterval(this.showNavbarOnScroll, 1000);
+    @ViewChild('logo')
+    private logo: ElementRef | undefined;
+    @ViewChild('navigation')
+    private navigation: ElementRef | undefined;
+
+    private obsOptions: {
+        root: null;
+        threshold: 0;
+    } | undefined;
+
+    ngOnInit(): void {
+        console.log(this.logo);
+        const observer = new IntersectionObserver(this.showNavbarOnScroll, this.obsOptions);
+        // @ts-ignore
+        observer.observe(document.getElementById('logo'));
     }
 
-    showNavbarOnScroll(): void
-    {
+
+    showNavbarOnScroll(entries: any): void {
         console.log('Cokolwiek');
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        if (document.documentElement.scrollTop > 20) {
             // @ts-ignore
             document.getElementById('navigation').style.top = '0';
+            console.log('if');
         }
-        else {
+        if (!entries.isIntersecting) {
             // @ts-ignore
             document.getElementById('navigation').style.top = '-5vw';
+            console.log('else');
         }
     }
 }
