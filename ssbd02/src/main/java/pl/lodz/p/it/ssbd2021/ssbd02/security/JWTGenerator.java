028d4f836af8db12b121cb22845ce0a74d6dda4c
224333 Kolanek Patryk <224333@edu.p.lodz.pl>
2021-04-04 21:24:16 +0200

    Add JWTGenerator with SecretConstants

---
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/security/JWTGenerator.java
@@ -0,0 +1,37 @@
+package pl.lodz.p.it.ssbd2021.ssbd02.security;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
+import javax.security.enterprise.identitystore.CredentialValidationResult;
+import java.util.Date;
+
+public class JWTGenerator {
+
+    public static String generateJWT(CredentialValidationResult credentialValidationResult) {
+        try {
+            JWSSigner jwsSigner = new MACSigner(SecretConstants.SECRET);
+
+            JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                    .subject(credentialValidationResult.getCallerPrincipal().getName())
+                    .claim(SecretConstants.AUTH, String.join(",", credentialValidationResult.getCallerGroups()))
+                    .issuer(SecretConstants.ISSUER)
+                    .expirationTime(new Date(new Date().getTime() + SecretConstants.EXPIRATION_TIME))
+                    .build();
+
+            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), jwtClaimsSet);
+
+            signedJWT.sign(jwsSigner);
+
+            return signedJWT.serialize();
+        } catch (JOSEException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
---
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/security/SecretConstants.java
@@ -0,0 +1,8 @@
+package pl.lodz.p.it.ssbd2021.ssbd02.security;
+
+public class SecretConstants {
+    public static final String SECRET = "_S_ARRxC9B7vQ739YOLM1LLc3HVJ4Z3_BxB7sOxubowKoNo5NiRDXrNAHRR2_ydXp7YD3EMXmSaVnKi74yIvELQ73GaSpUxsmg-hqXkatx-K4d7aimrQebggNejo4WxaMsmfNF7Mh2BIWE_hqAvNAyK-J4b5SnruXg7fcZ8qGus";
+    public static final String AUTH = "auth";
+    public static final String ISSUER = "ssbd02";
+    public static final int EXPIRATION_TIME = 10 * 60 * 1000;
+}
