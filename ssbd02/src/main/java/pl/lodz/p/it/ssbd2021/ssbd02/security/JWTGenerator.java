c9e972d8ffa1b3fadc4cf0e61216155b30852918
224333 Kolanek Patryk <224333@edu.p.lodz.pl>
2021-04-05 09:46:19 +0200

    Add JavaDoc

--- ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/security/JWTGenerator.java
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/security/JWTGenerator.java
@@ -11,11 +11,24 @@ import com.nimbusds.jwt.SignedJWT;
 import javax.security.enterprise.identitystore.CredentialValidationResult;
 import java.util.Date;
 
+/**
+ * Klasa generująca token JWT.
+ *
+ * @author Patryk Kolanek
+ */
 public class JWTGenerator {
 
+    /**
+     * Metoda generująca token JWT.
+     *
+     * @param credentialValidationResult Obiekt zawierający poświadczenia dla którego wygenerowany ma zostać token JWT.
+     * @return Wygenerowany token JWT.
+     */
     public static String generateJWT(CredentialValidationResult credentialValidationResult) {
         try {
             JWSSigner jwsSigner = new MACSigner(SecretConstants.SECRET);
+
+            JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS256);
 
             JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
                     .subject(credentialValidationResult.getCallerPrincipal().getName())
@@ -24,7 +37,7 @@ public class JWTGenerator {
                     .expirationTime(new Date(new Date().getTime() + SecretConstants.EXPIRATION_TIME))
                     .build();
 
-            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), jwtClaimsSet);
+            SignedJWT signedJWT = new SignedJWT(jwsHeader, jwtClaimsSet);
             signedJWT.sign(jwsSigner);
 
             return signedJWT.serialize();
--- ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/security/JWTVerifier.java
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/security/JWTVerifier.java
@@ -7,8 +7,19 @@ import com.nimbusds.jwt.SignedJWT;
 
 import java.text.ParseException;
 
+/**
+ * Klasa weryfikująca poprawność tokenu JWT.
+ *
+ * @author Patryk Kolanek
+ */
 public class JWTVerifier {
 
+    /**
+     * Metoda sprawdzająca czy token JWT jest poprawny.
+     *
+     * @param jwt Token JWT.
+     * @return Zwraca prawdę, gdy token jest poprawny, w przeciwnym razie fałsz.
+     */
     public static boolean validateJwt(String jwt) {
         try {
             SignedJWT signedJWT = SignedJWT.parse(jwt);
