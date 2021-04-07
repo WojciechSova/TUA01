f6e217f5945ba38ba24d6d092ef493be48f58107
Julia Kołodziej <224334@edu.p.lodz.pl>
2021-04-07 16:50:57 +0200

    Add JavaDoc, refactor methods

--- ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/facades/AbstractFacade.java
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/facades/AbstractFacade.java
@@ -1,60 +1,120 @@
 package pl.lodz.p.it.ssbd2021.ssbd02.facades;
 
+import lombok.AllArgsConstructor;
+
 import javax.persistence.*;
 import javax.persistence.criteria.*;
 import java.util.List;
 
+/**
+ * Klasa abstrakcyjna implementująca podstawowe operacje na encjach
+ *
+ * @param <T>
+ * @author Julia Kołodziej
+ */
+
+@AllArgsConstructor
 public abstract class AbstractFacade<T> {
 
     private final Class<T> entityClass;
 
-    public AbstractFacade(Class<T> entityClass) {
-        this.entityClass = entityClass;
-    }
-
     protected abstract EntityManager getEntityManager();
 
+    /**
+     * Metoda odpowiadająca ze dołączenie encji do kontekstu utrwalania i przejście encji w stan zarządzania
+     *
+     * @param entity Encja typu T
+     */
     public void create(T entity) {
         getEntityManager().persist(entity);
     }
 
-    public T find(Object id){
+    /**
+     * Metoda wyszukująca encję danej klasy o danej wartości klucza głównego
+     *
+     * @param id Klucz główny
+     * @return Encja typu T w przypadku zgodności wartości klucza głównego,
+     * null gdy brak encji o podanej wartości klucza głównego
+     */
+    public T find(Object id) {
         return getEntityManager().find(entityClass, id);
     }
 
-    public void edit(T entity){
+    /**
+     * Metoda synchronizująca stan obiektu w kontekście utrwalania, opowiada za utworzenie kopii przekazanej encji,
+     * i dołączenie jej do kontektu utrwalania
+     *
+     * @param entity Encja typu T
+     */
+    public void edit(T entity) {
         getEntityManager().merge(entity);
     }
 
-    public void remove(T entity){
+    /**
+     * Metoda usuwająca encję
+     *
+     * @param entity Encja typu T
+     */
+    public void remove(T entity) {
         getEntityManager().remove(getEntityManager().merge(entity));
     }
 
+    /**
+     * Metoda wyszukująca wszystkie encje danego typu
+     *
+     * @return Kolekcja wszystkich encji typu T
+     */
     public List<T> findAll() {
-        CriteriaQuery<T> cq = getEntityManager().getCriteriaBuilder().createQuery(entityClass);
-        cq.select(cq.from(entityClass));
-        return getEntityManager().createQuery(cq).getResultList();
+        CriteriaQuery<T> criteriaQuery = getEntityManager().getCriteriaBuilder().createQuery(entityClass);
+        Root<T> root = criteriaQuery.from(entityClass);
+        criteriaQuery.select(root);
+        return getEntityManager().createQuery(criteriaQuery).getResultList();
     }
 
-    public List<T> findRange(int[] range) {
-        CriteriaQuery<T> cq = getEntityManager().getCriteriaBuilder().createQuery(entityClass);
-        cq.select(cq.from(entityClass));
-        TypedQuery<T> q = getEntityManager().createQuery(cq);
-        q.setFirstResult(range[0]).setMaxResults(range[1] - range[0] + 1);
-        return q.getResultList();
+    /**
+     * Metoda pobierająca encje danego typu z podanego zakresu
+     *
+     * @param range Zakres
+     * @return Kolekcja encji typu T z przekazanego zakresu
+     */
+    public List<T> findInRange(int[] range) {
+        CriteriaQuery<T> criteriaQuery = getEntityManager().getCriteriaBuilder().createQuery(entityClass);
+        Root<T> root = criteriaQuery.from(entityClass);
+        criteriaQuery.select(root);
+        TypedQuery<T> query = getEntityManager().createQuery(criteriaQuery);
+        query.setFirstResult(range[0]).setMaxResults(range[1] - range[0] + 1);
+        return query.getResultList();
     }
 
+    /**
+     * Metoda zwracająca liczbę wszystkich encji danego typu
+     *
+     * @return Liczba wszystkich encji typu T
+     */
     public int count() {
-        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-        CriteriaQuery<Long> cq = cb.createQuery(Long.class);
-        cq.select(cb.count(cq.from(entityClass)));
-        return (getEntityManager().createQuery(cq).getSingleResult()).intValue();
+        CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<Long> criteriaQuery = criteriaBuilder.createQuery(Long.class);
+        Root<T> root = criteriaQuery.from(entityClass);
+        criteriaQuery.select(criteriaBuilder.count(root));
+        return (getEntityManager().createQuery(criteriaQuery).getSingleResult()).intValue();
     }
 
+    /**
+     * Metoda wyszukująca encje na podstawie przekazanego zapytania nazwanego
+     *
+     * @param namedQuery Zapytanie nazwane
+     * @return Kolekcja encji typu T
+     */
     public List<T> findWithNamedQuery(String namedQuery) {
         return getEntityManager().createNamedQuery(namedQuery, entityClass).getResultList();
     }
 
+    /**
+     * Metoda wyszukująca encje na podstawie przekazanego zapytania
+     *
+     * @param query Zapytanie
+     * @return Kolekcja encji typu T
+     */
     public List<T> findWithQuery(String query) {
         return getEntityManager().createQuery(query, entityClass).getResultList();
     }
