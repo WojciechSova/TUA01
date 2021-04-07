e2a2aa32635f3a5703c24129bcd6e29187cbcdf8
Kacper Świercz <224443@edu.p.lodz.pl>
2021-04-07 15:42:21 +0200

    Add authorization facades

---
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/facades/auth/AccessLevelFacade.java
@@ -0,0 +1,46 @@
+package pl.lodz.p.it.ssbd2021.ssbd02.facades.auth;
+
+import pl.lodz.p.it.ssbd2021.ssbd02.facades.AbstractFacade;
+import pl.lodz.p.it.ssbd2021.ssbd02.model.AccessLevel;
+import pl.lodz.p.it.ssbd2021.ssbd02.model.Account;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+/**
+ * Klasa rozszerzająca abstrakcyjną klasę {@link AbstractFacade}.
+ * Używa konkretnego typu {@link AccessLevel} zamiast typu generycznego.
+ * Jednostka składowania używana do wstrzyknięcia zarządcy encji to ssbd02authPU.
+ *
+ * @author Kacper Świercz
+ */
+@Stateless
+public class AccessLevelFacade extends AbstractFacade<AccessLevel> {
+
+    @PersistenceContext(unitName = "ssbd02authPU")
+    private EntityManager entityManager;
+
+    public AccessLevelFacade() {
+        super(AccessLevel.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    /**
+     * Metoda wyszukująca encje typu {@link AccessLevel}, w których znajduje się dowiązanie do encji typu {@link Account} o podanym loginie.
+     *
+     * @param login Login encji.
+     * @return Zwraca kolekcje encji typu {@link AccessLevel}, w których znajduje się dowiązanie do encji typu {@link Account} o podanym loginie.
+     */
+    public List<AccessLevel> findByLogin(String login) {
+        TypedQuery<AccessLevel> tq = entityManager.createNamedQuery("AccessLevel.findByLogin", AccessLevel.class);
+        tq.setParameter("login", login);
+        return tq.getResultList();
+    }
+}
---
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/facades/auth/AccountFacade.java
@@ -0,0 +1,45 @@
+package pl.lodz.p.it.ssbd2021.ssbd02.facades.auth;
+
+import pl.lodz.p.it.ssbd2021.ssbd02.facades.AbstractFacade;
+import pl.lodz.p.it.ssbd2021.ssbd02.model.AccessLevel;
+import pl.lodz.p.it.ssbd2021.ssbd02.model.Account;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ * Klasa rozszerzająca abstrakcyjną klasę {@link AbstractFacade}.
+ * Używa konkretnego typu {@link Account} zamiast typu generycznego.
+ * Jednostka składowania używana do wstrzyknięcia zarządcy encji to ssbd02authPU.
+ *
+ * @author Kacper Świercz
+ */
+@Stateless
+public class AccountFacade extends AbstractFacade<Account> {
+
+    @PersistenceContext(unitName = "ssbd02authPU")
+    private EntityManager entityManager;
+
+    public AccountFacade() {
+        super(Account.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    /**
+     * Metoda wyszukująca encję typu {@link Account} o podanym loginie.
+     *
+     * @param login Login encji.
+     * @return Zwraca encję typu {@link Account} o podanym loginie.
+     */
+    public Account findByLogin(String login) {
+        TypedQuery<Account> tq = entityManager.createNamedQuery("Account.findByLogin", Account.class);
+        tq.setParameter("login", login);
+        return tq.getSingleResult();
+    }
+}
