607237d6ebf23861b07fb010713a56998c37099c
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-04-08 13:49:52 +0200

    Implement mop facades

---
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/facades/mop/VehicleTypeFacade.java
@@ -0,0 +1,31 @@
+package pl.lodz.p.it.ssbd2021.ssbd02.facades.mop;
+
+import pl.lodz.p.it.ssbd2021.ssbd02.facades.AbstractFacade;
+import pl.lodz.p.it.ssbd2021.ssbd02.model.mop.VehicleType;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Klasa rozszerzająca abstrakcyjną klasę {@link AbstractFacade}.
+ * Używa konkretnego typu {@link VehicleType} zamiast typu generycznego.
+ * Jednostka składowania używana do wstrzyknięcia zarządcy encji do ssbd02mopPU.
+ *
+ * @author Artur Madaj
+ */
+@Stateless
+public class VehicleTypeFacade extends AbstractFacade<VehicleType> {
+
+    @PersistenceContext(unitName = "ssbd02mopPU")
+    private EntityManager entityManager;
+
+    public VehicleTypeFacade() {
+        super(VehicleType.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return entityManager;
+    }
+}
