4e6a1b3175022320e6b05b194fd21809ea651002
Julia Kołodziej <224334@edu.p.lodz.pl>
2021-04-20 16:33:49 +0200

    Add active field to AccountGeneral

--- spa/src/app/model/mok/AccountGeneral.ts
+++ spa/src/app/model/mok/AccountGeneral.ts
@@ -2,6 +2,7 @@ import {AccessLevel} from './AccessLevel';
 
 export interface AccountGeneral {
     login: string;
+    active: boolean;
     firstName: string;
     lastName: string;
     accessLevel: AccessLevel[];
