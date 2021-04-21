26e3696ae35adc24aa877fc4e3026f26fdcfc3c9
224333 Kolanek Patryk <224333@edu.p.lodz.pl>
2021-04-21 09:52:01 +0200

    Move main page to main component

--- spa/src/app/app-routing.module.ts
+++ spa/src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './common/login/login.component';
+import { RegisterComponent } from './common/register/register.component';
+import { MainPageComponent } from './main/main-page/main-page.component';
+
+const mainChildren: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
+];
 
 const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'main_page', component: AppComponent }
+    { path: '', component: MainPageComponent, children: mainChildren }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
