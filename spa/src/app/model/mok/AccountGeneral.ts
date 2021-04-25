0d67d65c111a55c87661c0c747ae4af5ab6642df
Wojciech Sowa <224429@edu.p.lodz.pl>
2021-04-25 09:25:53 +0200

    Change type of accessLevel in AccountGeneralDTO and in SPA AccountGeneral

--- spa/src/app/model/mok/AccountGeneral.ts
+++ spa/src/app/model/mok/AccountGeneral.ts
@@ -1,9 +1,7 @@
-import {AccessLevel} from './AccessLevel';
-
 export interface AccountGeneral {
     login: string;
     active: boolean;
     firstName: string;
     lastName: string;
-    accessLevel: AccessLevel[];
+    accessLevel: String[];
 }
