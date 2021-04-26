e2e9530944d397803b2355c2bf2f21373111e9a8
Wojciech Sowa <224429@edu.p.lodz.pl>
2021-04-27 01:37:54 +0200

    Add mappers for MOK
    Add dependency for Pair<>
    Add annotation @EqualsAndHashCode in DTO for testing purpose

---
+++ ssbd02/src/test/java/pl/lodz/p/it/ssbd2021/ssbd02/utils/mappers/AccessLevelMapperTest.java
@@ -0,0 +1,44 @@
+package pl.lodz.p.it.ssbd2021.ssbd02.utils.mappers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.lodz.p.it.ssbd2021.ssbd02.dto.mok.AccessLevelDTO;
+import pl.lodz.p.it.ssbd2021.ssbd02.entities.mok.AccessLevel;
+import pl.lodz.p.it.ssbd2021.ssbd02.entities.mok.Account;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccessLevelMapperTest {
+    AccessLevel accessLevel;
+    Account accountModifiedBy;
+    Account accountCreatedBy;
+
+    @BeforeEach
+    void setUp() {
+        accountModifiedBy = new Account();
+        accountModifiedBy.setLogin("ModifiedLogin");
+        accountCreatedBy = new Account();
+        accountCreatedBy.setLogin("CreatedLogin");
+        accessLevel = new AccessLevel();
+        accessLevel.setLevel("ADMIN");
+        accessLevel.setModificationDate(Timestamp.from(Instant.now()));
+        accessLevel.setModifiedBy(accountModifiedBy);
+        accessLevel.setCreationDate(Timestamp.valueOf("2020-03-25 11:21:15"));
+        accessLevel.setCreatedBy(accountCreatedBy);
+    }
+
+    @Test
+    void createAccessLevelDTOFromEntity() {
+        AccessLevelDTO accLvlDTO = AccessLevelMapper.createAccessLevelDTOFromEntity(accessLevel);
+
+        assertEquals(accessLevel.getLevel(), accLvlDTO.getLevel());
+        assertTrue(accLvlDTO.getActive());
+        assertEquals(accessLevel.getModificationDate(), accLvlDTO.getModificationDate());
+        assertEquals(AccountMapper.createAccountGeneralDTOFromEntity(accessLevel.getModifiedBy()).hashCode(), accLvlDTO.getModifiedBy().hashCode());
+        assertEquals(accessLevel.getCreationDate(), accLvlDTO.getCreationDate());
+        assertEquals(AccountMapper.createAccountGeneralDTOFromEntity(accessLevel.getCreatedBy()).hashCode(), accLvlDTO.getCreatedBy().hashCode());
+    }
+}
