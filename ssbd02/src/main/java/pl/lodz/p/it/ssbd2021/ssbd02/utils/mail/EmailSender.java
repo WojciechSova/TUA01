932dac3070dc2b8c4993afab0cf2c59acfde80fb
Karolina Kowalczyk <224340@edu.p.lodz.pl>
2021-04-27 19:47:01 +0200

    Add email sending class and mail.properties

---
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/utils/mail/EmailSender.java
@@ -0,0 +1,102 @@
+package pl.lodz.p.it.ssbd2021.ssbd02.utils.mail;
+
+import org.simplejavamail.api.email.*;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Klasa implementująca mechanizm wysyłania wiadomości email.
+ *
+ * @author Karolina Kowalczyk
+ */
+public class EmailSender {
+
+    /**
+     * Metoda wysyłająca wiadomość email z linkiem pozwalającym na potwierdzenie nowo założonego konta.
+     *
+     * @param recipientName         Imię odbiorcy wiadomości.
+     * @param recipientEmailAddress Adres email odbiorcy wiadomości.
+     */
+    public static void sendRegistrationEmail(String recipientName, String recipientEmailAddress, String link) {
+        try (InputStream input = new FileInputStream("src/main/resources/mail.properties")) {
+
+            Properties prop = new Properties();
+
+            prop.load(input);
+
+            String htmlText = prop.getProperty("mail.registration.text").replace("CONFIRMATION_LINK", link);
+            String subject = prop.getProperty("mail.registration.subject");
+            sendEmail(recipientName, recipientEmailAddress, subject, htmlText);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Metoda wysyłająca wiadomość email informującą użytkownika o modyfikacji danych jego konta.
+     *
+     * @param recipientName         Imię odbiorcy wiadomości.
+     * @param recipientEmailAddress Adres email odbiorcy wiadomości.
+     */
+    public static void sendModificationEmail(String recipientName, String recipientEmailAddress) {
+        try (InputStream input = new FileInputStream("src/main/resources/mail.properties")) {
+
+            Properties prop = new Properties();
+
+            prop.load(input);
+
+            String htmlText = prop.getProperty("mail.info.modification.text");
+            String subject = prop.getProperty("mail.info.modification.subject");
+            sendEmail(recipientName, recipientEmailAddress, subject, htmlText);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Metoda wysyłająca wiadomość email.
+     *
+     * @param recipientName         Imię odbiorcy wiadomości.
+     * @param recipientEmailAddress Adres email odbiorcy wiadomości.
+     * @param subject               Temat wiadomości.
+     * @param text                  Treść wiadomości.
+     */
+    public static void sendEmail(String recipientName, String recipientEmailAddress, String subject, String text) {
+        try (InputStream input = new FileInputStream("src/main/resources/mail.properties")) {
+
+            Properties prop = new Properties();
+
+            prop.load(input);
+
+            String username = prop.getProperty("mail.ferrytales.username");
+            String password = prop.getProperty("mail.ferrytales.password");
+            String emailAddress = prop.getProperty("mail.ferrytales.address");
+
+            Email email = EmailBuilder.startingBlank()
+                    .from("Ferrytales", emailAddress)
+                    .to(recipientName, recipientEmailAddress)
+                    .withSubject(subject)
+                    .withHTMLText(text)
+                    .buildEmail();
+
+            Mailer mailer = MailerBuilder
+                    .withSMTPServer("in-v3.mailjet.com", 587, username, password)
+                    .withTransportStrategy(TransportStrategy.SMTP)
+                    .buildMailer();
+
+            mailer.sendMail(email);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
