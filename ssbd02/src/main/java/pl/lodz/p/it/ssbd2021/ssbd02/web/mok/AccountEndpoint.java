eb60a6633138884390e36430b72c80f79ed1d97c
Daniel Łondka <224360@edu.p.lodz.pl>
2021-04-28 13:43:34 +0200

    Add AccountEndpoint getAllAccountGenerals method

---
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/web/mok/AccountEndpoint.java
@@ -0,0 +1,42 @@
+package pl.lodz.p.it.ssbd2021.ssbd02.web.mok;
+
+import pl.lodz.p.it.ssbd2021.ssbd02.dto.mok.AccountGeneralDTO;
+import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mok.managers.interfaces.AccountManagerLocal;
+import pl.lodz.p.it.ssbd2021.ssbd02.utils.mappers.AccountMapper;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Klasa ziarna CDI o zasięgu żądania.
+ * Zawiera metody zawierające zapytania związane z modułem obsługi kont.
+ *
+ * @author Daniel Łondka
+ */
+@RequestScoped
+@Path("/account")
+public class AccountEndpoint {
+
+    @Inject
+    private AccountManagerLocal accountManager;
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/general")
+    public Response getAllAccountGenerals() {
+        List<AccountGeneralDTO> accountGeneralDTOList = accountManager.getAllAccountsWithAccessLevels().stream()
+                .map(AccountMapper::createAccountGeneralDTOFromEntities)
+                .collect(Collectors.toList());
+        return Response.accepted(accountGeneralDTOList)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .entity(accountGeneralDTOList)
+                .build();
+    }
+}
