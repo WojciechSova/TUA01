f6ae0eb10e4bf42bdb5a215f3cebc9cb84cca037
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-04-29 20:56:30 +0200

    Implement registration endpoint

--- ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/web/mok/AccountEndpoint.java
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/web/mok/AccountEndpoint.java
@@ -1,14 +1,13 @@
 package pl.lodz.p.it.ssbd2021.ssbd02.web.mok;
 
+import pl.lodz.p.it.ssbd2021.ssbd02.dto.mok.AccountDetailsDTO;
 import pl.lodz.p.it.ssbd2021.ssbd02.dto.mok.AccountGeneralDTO;
 import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mok.managers.interfaces.AccountManagerLocal;
 import pl.lodz.p.it.ssbd2021.ssbd02.utils.mappers.AccountMapper;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -40,6 +39,18 @@ public class AccountEndpoint {
                 .collect(Collectors.toList());
         return Response.accepted(accountGeneralDTOList)
                 .entity(accountGeneralDTOList)
+                .build();
+    }
+
+    @POST
+    @Path("register")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createAccount(AccountDetailsDTO accountDTO) {
+        if (accountDTO.getPassword() == null || accountDTO.getEmail() == null || accountDTO.getLogin() == null) {
+            throw new WebApplicationException("Not all required fields were provided", 400);
+        }
+        accountManager.createAccount(AccountMapper.createAccountFromAccountDetailsDTO(accountDTO));
+        return Response.accepted()
                 .build();
     }
 }
