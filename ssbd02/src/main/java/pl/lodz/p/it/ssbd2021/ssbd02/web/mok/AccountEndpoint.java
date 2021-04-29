c84be4ee2e4a167d282d4b4a2a55ed3353c6b15c
Kacper Świercz <224443@edu.p.lodz.pl>
2021-04-29 08:59:14 +0200

    Add profile endpoint

--- ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/web/mok/AccountEndpoint.java
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/web/mok/AccountEndpoint.java
@@ -1,16 +1,20 @@
 package pl.lodz.p.it.ssbd2021.ssbd02.web.mok;
 
+import pl.lodz.p.it.ssbd2021.ssbd02.dto.mok.AccountDetailsDTO;
 import pl.lodz.p.it.ssbd2021.ssbd02.dto.mok.AccountGeneralDTO;
 import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mok.managers.interfaces.AccountManagerLocal;
 import pl.lodz.p.it.ssbd2021.ssbd02.utils.mappers.AccountMapper;
 
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -40,6 +44,18 @@ public class AccountEndpoint {
                 .collect(Collectors.toList());
         return Response.accepted(accountGeneralDTOList)
                 .entity(accountGeneralDTOList)
+                .build();
+    }
+
+    @GET
+    @RolesAllowed({"ADMIN", "EMPLOYEE", "CLIENT"})
+    @Path("/profile")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getProfile(@Context SecurityContext securityContext) {
+        AccountDetailsDTO account = AccountMapper.createAccountDetailsDTOFromEntities(
+                accountManager.getAccountWithLogin(securityContext.getUserPrincipal().getName())
+        );
+        return Response.ok(account)
                 .build();
     }
 }
