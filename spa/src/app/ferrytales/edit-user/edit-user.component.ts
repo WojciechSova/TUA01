81b040fdc47eea86dd494568a258829027ed7d76
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-05-03 21:46:44 +0200

    Add html, css & init component

--- spa/src/app/ferrytales/edit-user/edit-user.component.ts
+++ spa/src/app/ferrytales/edit-user/edit-user.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountDetails} from "../../model/mok/AccountDetails";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { validateEmail } from "../../common/navigation/register/matching.validator";
 
 @Component({
-  selector: 'app-edit-user',
-  templateUrl: './edit-user.component.html',
-  styleUrls: ['./edit-user.component.less']
+    selector: 'app-edit-user',
+    templateUrl: './edit-user.component.html',
+    styleUrls: ['./edit-user.component.less']
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+    public user: AccountDetails = {
+        accessLevel: [],
+        active: false,
+        confirmed: false,
+        creationDate: new Date(),
+        email: '',
+        firstName: '',
+        lastName: '',
+        numberOfBadLogins: 0,
+        login: '',
+        password: '',
+        phoneNumber: '',
+        language: '',
+        timeZone: ''
+    };
 
-  ngOnInit(): void {
-  }
+    public timezones: string[] = [
+        'GMT-12',
+        'GMT-11',
+        'GMT-10',
+        'GMT-9',
+        'GMT-8',
+        'GMT-7',
+        'GMT-6',
+        'GMT-5',
+        'GMT-4',
+        'GMT-3',
+        'GMT-2',
+        'GMT-1',
+        'GMT+0',
+        'GMT+1',
+        'GMT+2',
+        'GMT+3',
+        'GMT+4',
+        'GMT+5',
+        'GMT+6',
+        'GMT+7',
+        'GMT+8',
+        'GMT+9',
+        'GMT+10',
+        'GMT+11',
+        'GMT+12'
+        ]
+
+    form = new FormGroup({
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        phoneNumber: new FormControl('', [Validators.pattern('[0-9]{11}')]),
+        email: new FormControl('', Validators.email),
+        language: new FormControl(''),
+        timeZone: new FormControl('')
+    });
+
+    constructor() {
+    }
+
+    editUser(firstName?: string, lastName?: string, phoneNumber?: string, email?: string, language?: string, timeZone?: string): void {
+    }
+
+    ngOnInit(): void {
+    }
 
 }
