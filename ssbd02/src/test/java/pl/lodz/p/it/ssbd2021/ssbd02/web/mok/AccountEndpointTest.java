1d35cc5f67bf9c1392bc3067999bb801665c1ad2
Daniel Łondka <224360@edu.p.lodz.pl>
2021-05-06 23:11:16 +0200

    Remove unnecesary import

--- ssbd02/src/test/java/pl/lodz/p/it/ssbd2021/ssbd02/web/mok/AccountEndpointTest.java
+++ ssbd02/src/test/java/pl/lodz/p/it/ssbd2021/ssbd02/web/mok/AccountEndpointTest.java
@@ -19,7 +19,6 @@ import java.nio.file.attribute.UserPrincipal;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
