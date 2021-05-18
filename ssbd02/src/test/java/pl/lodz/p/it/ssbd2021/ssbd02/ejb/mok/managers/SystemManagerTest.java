5f5e90741eaa74bb1def890a9582441806848652
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-05-18 20:02:24 +0200

    Fix tests

--- ssbd02/src/test/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mok/managers/SystemManagerTest.java
+++ ssbd02/src/test/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mok/managers/SystemManagerTest.java
@@ -19,6 +19,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -43,6 +44,10 @@ class SystemManagerTest {
     private final String mail2 = "ferrytales@protonmail.com";
     private final String name3 = "Artur3";
     private final String mail3 = "ferrytales@protonmail.com";
+    private final OneTimeUrl url1 = new OneTimeUrl();
+    private final OneTimeUrl url2 = new OneTimeUrl();
+    private final OneTimeUrl url3 = new OneTimeUrl();
+
     @Mock
     private AccountFacadeLocal accountFacadeLocal;
     @Mock
@@ -55,6 +60,8 @@ class SystemManagerTest {
     private SystemManager systemManager;
     private List<AccessLevel> accessLevels;
     private List<Account> accounts;
+    private List<OneTimeUrl> urls;
+    private int expirationTime = 86400;
 
 
     @BeforeEach
@@ -71,6 +78,7 @@ class SystemManagerTest {
 
         accessLevels = new ArrayList<>();
         accounts = new ArrayList<>();
+        urls = new ArrayList<>();
 
         al1.setAccount(a1);
         al2.setAccount(a2);
@@ -78,16 +86,14 @@ class SystemManagerTest {
 
         accessLevels.addAll(List.of(al1, al2, al3));
         accounts.addAll(List.of(a1, a2, a3));
+        urls.addAll(List.of(url1, url2, url3));
     }
 
     @Test
     void removeUnconfirmedAccountsTest() {
-        when(a1.getCreationDate())
-                .thenReturn(Timestamp.from(Instant.now().minus(Duration.ofHours(24).plus(Duration.ofSeconds(1)))));
-        when(a2.getCreationDate())
-                .thenReturn(Timestamp.from(Instant.now().minus(Duration.ofHours(24).plus(Duration.ofMinutes(5)))));
-        when(a3.getCreationDate())
-                .thenReturn(Timestamp.from(Instant.now().minus(Duration.ofHours(23).plus(Duration.ofMinutes(59)))));
+        a1.setCreationDate((Timestamp.from(Instant.now().minus(Duration.ofHours(24).plus(Duration.ofSeconds(1))))));
+        a2.setCreationDate((Timestamp.from(Instant.now().minus(Duration.ofHours(24).plus(Duration.ofMinutes(5))))));
+        a3.setCreationDate((Timestamp.from(Instant.now().minus(Duration.ofHours(23).plus(Duration.ofMinutes(59))))));
 
         when(a1.getFirstName()).thenReturn(name1);
         when(a1.getEmail()).thenReturn(mail1);
@@ -96,8 +102,12 @@ class SystemManagerTest {
         when(a3.getFirstName()).thenReturn(name3);
         when(a3.getEmail()).thenReturn(mail3);
 
-        when(accountFacadeLocal.findByConfirmed(false)).thenReturn(accounts);
+        doReturn(accounts.stream()
+                .filter(account -> Timestamp.from(Instant.now().minus(Duration.ofSeconds(account.getCreationDate().getTime() / 1000))).getTime() / 1000 > expirationTime)
+                .collect(Collectors.toList())).when(accountFacadeLocal).findByUnconfirmedAndExpired(expirationTime);
         when(accessLevelFacadeLocal.findAllByAccount(a1)).thenReturn(List.of(al1, al2));
+        when(oneTimeUrlFacadeLocal.findByAccount(a1)).thenReturn(List.of(url1));
+        when(oneTimeUrlFacadeLocal.findByAccount(a2)).thenReturn(List.of(url2));
 
         doAnswer(invocationOnMock -> {
             accessLevels.remove(al1);
@@ -125,13 +135,28 @@ class SystemManagerTest {
             return null;
         }).when(accountFacadeLocal).remove(a3);
 
+        doAnswer(invocationOnMock -> {
+            urls.remove(url1);
+            return null;
+        }).when(oneTimeUrlFacadeLocal).remove(url1);
+        doAnswer(invocationOnMock -> {
+            urls.remove(url2);
+            return null;
+        }).when(oneTimeUrlFacadeLocal).remove(url2);
+        doAnswer(invocationOnMock -> {
+            urls.remove(url3);
+            return null;
+        }).when(oneTimeUrlFacadeLocal).remove(url3);
+
         systemManager.removeUnconfirmedAccounts();
 
         assertEquals(1, accounts.size());
         assertEquals(1, accessLevels.size());
+        assertEquals(1, urls.size());
 
         assertEquals(a3, accounts.get(0));
         assertEquals(al3, accessLevels.get(0));
+        assertEquals(url3, urls.get(0));
     }
 
     @Test
