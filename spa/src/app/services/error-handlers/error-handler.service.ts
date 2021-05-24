ab05ab9c1cc7d46629001240d2f3261881dcc29d
224333 Kolanek Patryk <224333@edu.p.lodz.pl>
2021-05-24 19:55:46 +0200

    Remove error pages for payara, handle 401 error

--- spa/src/app/services/error-handlers/error-handler.service.ts
+++ spa/src/app/services/error-handlers/error-handler.service.ts
@@ -13,12 +13,18 @@ export class ErrorHandlerService implements ErrorHandler {
     }
 
     handleError(error: HttpErrorResponse): Observable<never> {
-        if (error.status === 403) {
+        if (error.status === 401) {
+            this.zone.run(() => this.router.navigateByUrl('/'));
+            return throwError('401 Unauthorized');
+        } else if (error.status === 403) {
             this.zone.run(() => this.router.navigateByUrl('/error/forbidden'));
             return throwError('403 Forbidden');
         } else if (error.status === 404) {
             this.zone.run(() => this.router.navigateByUrl('error/notfound'));
             return throwError('404 Not Found');
+        } else if (error.status === 500) {
+            this.zone.run(() => this.router.navigateByUrl('error/internal'));
+            return throwError('500 Internal Error');
         }
 
         return throwError('Undefined error.');
