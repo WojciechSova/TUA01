b92998ee714d44f65ca05b27ad883f8435429455
Daniel Łondka <224360@edu.p.lodz.pl>
2021-05-24 12:01:36 +0000

    ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mok/facades/interfaces/AccountFacadeLocal.java edited online with Bitbucket
    Fix javadoc typo

--- ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mok/facades/interfaces/AccountFacadeLocal.java
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mok/facades/interfaces/AccountFacadeLocal.java
@@ -54,7 +54,7 @@ public interface AccountFacadeLocal extends AbstractFacadeInterface<Account> {
      * Metoda wyszukująca listę encji typu {@link Account} o przekazanym adresie email.
      *
      * @param email Adres email encji
-     * @return Lista obietków typu {@link Account} o przekazanym adresie email
+     * @return Lista obiektów typu {@link Account} o przekazanym adresie email
      */
     List<Account> findListByEmail(String email);
 }
