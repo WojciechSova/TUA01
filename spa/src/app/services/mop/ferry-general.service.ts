5511d266eb32c37bf282d7eb028194d8ee9dbc31
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-06-04 22:26:42 +0200

    Connect endpoint with spa

--- spa/src/app/services/mop/ferry-general.service.ts
+++ spa/src/app/services/mop/ferry-general.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, OnDestroy } from "@angular/core";
-import { FerryGeneral } from "../../model/mop/FerryGeneral";
-import { HttpClient } from "@angular/common/http";
+import { Injectable, OnDestroy } from '@angular/core';
+import { FerryGeneral } from '../../model/mop/FerryGeneral';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -8,12 +9,17 @@ import { HttpClient } from "@angular/common/http";
 export class FerryGeneralService implements OnDestroy {
 
     ferriesGeneralList: FerryGeneral[] = [];
+    private readonly url: string;
 
     constructor(private http: HttpClient) {
+        this.url = environment.appUrl + '/ferries'
     }
 
     getFerries(): any {
-
+        return this.http.get<any>(this.url, {
+            observe: 'body',
+            responseType: 'json'
+        });
     }
 
     ngOnDestroy(): void {
