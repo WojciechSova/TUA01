334e137ed20ca21d694d9a46d5b54549bc98f6d4
Karolina Kowalczyk <224340@edu.p.lodz.pl>
2021-06-05 11:34:54 +0200

    Connect view with endpoint for seaports list

--- spa/src/app/ferrytales/seaports-table/seaports-table.component.ts
+++ spa/src/app/ferrytales/seaports-table/seaports-table.component.ts
@@ -1,26 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SeaportGeneral } from '../../model/mop/SeaportGeneral';
+import { SeaportGeneralService } from '../../services/mop/seaport-general.service';
 
 @Component({
-  selector: 'app-seaports-table',
-  templateUrl: './seaports-table.component.html',
-  styleUrls: ['./seaports-table.component.less']
+    selector: 'app-seaports-table',
+    templateUrl: './seaports-table.component.html',
+    styleUrls: ['./seaports-table.component.less']
 })
 export class SeaportsTableComponent implements OnInit {
 
-  constructor(private router: Router) { }
+    constructor(private router: Router,
+                private seaportGeneralService: SeaportGeneralService) {
+        this.getSeaports();
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
     goToHomeBreadcrumb(): void {
         this.router.navigate(['/']);
     }
 
-    listSeaports(): SeaportGeneral[] | null {
-        return null;
+    getSeaports(): void {
+        this.seaportGeneralService.getSeaports().subscribe(
+            (response: SeaportGeneral[]) => {
+                this.seaportGeneralService.seaportsList = response;
+            }
+        );
     }
 
-    getSeaports(): void {}
+    listSeaports(): SeaportGeneral[] {
+        return this.seaportGeneralService.seaportsList;
+    }
+
 }
