eb8c23bbff355d6e4a68fd31a608c7b5f1f138d4
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-06-06 09:50:59 +0200

    Implement create() method for seaports

--- ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mop/facades/SeaportFacade.java
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mop/facades/SeaportFacade.java
@@ -5,6 +5,7 @@ import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mop.facades.interfaces.SeaportFacadeLoca
 import pl.lodz.p.it.ssbd2021.ssbd02.entities.mop.Seaport;
 import pl.lodz.p.it.ssbd2021.ssbd02.utils.interceptors.GeneralInterceptor;
 import pl.lodz.p.it.ssbd2021.ssbd02.utils.interceptors.PersistenceInterceptor;
+import pl.lodz.p.it.ssbd2021.ssbd02.utils.interceptors.TrackerInterceptor;
 import pl.lodz.p.it.ssbd2021.ssbd02.utils.interceptors.mop.SeaportInterceptor;
 
 import javax.annotation.security.DenyAll;
@@ -28,7 +29,7 @@ import java.util.List;
 @Stateless
 @TransactionAttribute(TransactionAttributeType.MANDATORY)
 @RolesAllowed({"DEFINITELY_NOT_A_REAL_ROLE"})
-@Interceptors({GeneralInterceptor.class, SeaportInterceptor.class, PersistenceInterceptor.class})
+@Interceptors({GeneralInterceptor.class, SeaportInterceptor.class, PersistenceInterceptor.class, TrackerInterceptor.class})
 public class SeaportFacade extends AbstractFacade<Seaport> implements SeaportFacadeLocal {
 
     @PersistenceContext(unitName = "ssbd02mopPU")
