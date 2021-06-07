49ce6823bc8a4d0a569c4f1a89e65aff9f6c4927
224333 Kolanek Patryk <224333@edu.p.lodz.pl>
2021-06-07 16:05:59 +0200

    Add missing information about booking

--- spa/src/app/ferrytales/booking-details/booking-details.component.ts
+++ spa/src/app/ferrytales/booking-details/booking-details.component.ts
@@ -16,13 +16,46 @@ export class BookingDetailsComponent implements OnInit {
         account: {
             login: 'Login',
             active: true,
-            firstName: '',
-            lastName: '',
+            firstName: 'First name',
+            lastName: 'Last Name',
             accessLevel: ['']
         },
-        cabin: 'cabin',
+        cabin: {
+            capacity: 'capacity',
+            cabinType: 'cabin type',
+            number: '123'
+        },
         creationDate: new Date(),
-        cruise: 'Cruise',
+        cruise: {
+            startDate: new Date(),
+            endDate: new Date(),
+            route: {
+                start: {
+                    code: '123',
+                    city: 'A'
+                },
+                destination: {
+                    code: '1234',
+                    city: 'B'
+                },
+                code: '123'
+            },
+            ferry: {
+                name: '123',
+                onDeckCapacity: 123,
+                vehicleCapacity: 123
+            },
+            number: '123',
+            modificationDate: new Date(),
+            creationDate: new Date(),
+            createdBy: {
+                login: 'Login',
+                active: true,
+                firstName: '',
+                lastName: '',
+                accessLevel: ['']
+            },
+        },
         number: '1234567890',
         numberOfPeople: 1,
         price: 23.43,
@@ -47,13 +80,46 @@ export class BookingDetailsComponent implements OnInit {
             account: {
                 login: 'Login',
                 active: true,
-                firstName: '',
-                lastName: '',
+                firstName: 'First name',
+                lastName: 'Last name',
                 accessLevel: ['']
             },
-            cabin: 'cabin',
+            cabin: {
+                capacity: 'capacity',
+                cabinType: 'cabin type',
+                number: '123'
+            },
             creationDate: new Date(),
-            cruise: 'Cruise',
+            cruise: {
+                startDate: new Date(),
+                endDate: new Date(),
+                route: {
+                    start: {
+                        code: '123',
+                        city: 'A'
+                    },
+                    destination: {
+                        code: '1234',
+                        city: 'B'
+                    },
+                    code: '123'
+                },
+                ferry: {
+                    name: '123',
+                    onDeckCapacity: 123,
+                    vehicleCapacity: 123
+                },
+                number: '123',
+                modificationDate: new Date(),
+                creationDate: new Date(),
+                createdBy: {
+                    login: 'Login',
+                    active: true,
+                    firstName: '',
+                    lastName: '',
+                    accessLevel: ['']
+                },
+            },
             number: '1234567890',
             numberOfPeople: 1,
             price: 23.43,
--- spa/src/app/model/mop/BookingDetails.ts
+++ spa/src/app/model/mop/BookingDetails.ts
@@ -1,11 +1,13 @@
 import { AccountGeneral } from '../mok/AccountGeneral';
 import { VehicleType } from './VehicleType';
+import { CabinGeneral } from './CabinGeneral';
+import { CruiseDetails } from './CruiseDetails';
 
 export interface BookingDetails {
-    cruise: string; // TODO change to CruiseGeneral
+    cruise: CruiseDetails;
     account: AccountGeneral;
     numberOfPeople: number;
-    cabin?: string; // TODO change to CabinGeneral
+    cabin?: CabinGeneral;
     vehicleType: VehicleType;
     price: number;
     number: string;
