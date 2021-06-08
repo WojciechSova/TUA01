611370bf8f775b3132d7199c1cd3aa6aab146784
Daniel Łondka <224360@edu.p.lodz.pl>
2021-06-08 20:18:03 +0200

    Add seaport-edit component

---
+++ spa/src/app/ferrytales/seaport-details/seaport-edit/seaport-edit.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seaport-edit',
+  templateUrl: './seaport-edit.component.html',
+  styleUrls: ['./seaport-edit.component.less']
+})
+export class SeaportEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+    changeSeaport() {
+
+    }
+
+    closeComponent() {
+
+    }
+}
