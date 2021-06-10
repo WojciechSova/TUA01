85ddd2e44d9e632f21c05a995fb403b0ccd7051b
Karolina Kowalczyk <224340@edu.p.lodz.pl>
2021-06-10 16:05:55 +0200

    Add CabinTypeManager test

---
+++ ssbd02/src/test/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mop/managers/CabinTypeManagerTest.java
@@ -0,0 +1,41 @@
+package pl.lodz.p.it.ssbd2021.ssbd02.ejb.mop.managers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mop.facades.interfaces.AccountMopFacadeLocal;
+import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mop.facades.interfaces.CabinTypeFacadeLocal;
+import pl.lodz.p.it.ssbd2021.ssbd02.entities.mop.Cabin;
+import pl.lodz.p.it.ssbd2021.ssbd02.entities.mop.CabinType;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class CabinTypeManagerTest {
+
+    @Mock
+    CabinTypeFacadeLocal cabinTypeFacadeLocal;
+
+    @InjectMocks
+    CabinTypeManager cabinTypeManager;
+
+    @Spy
+    CabinType cabinType = new CabinType();
+
+    @BeforeEach
+    void initMocks() {
+        MockitoAnnotations.openMocks(this);
+        cabinType.setCabinTypeName("Nazwa");
+
+    }
+
+    @Test
+    void getCabinTypeByName() {
+        when(cabinTypeFacadeLocal.findByName("Nazwa")).thenReturn(cabinType);
+        assertEquals(cabinType, cabinTypeManager.getCabinTypeByName("Nazwa"));
+        assertEquals(cabinType.hashCode(), cabinTypeManager.getCabinTypeByName("Nazwa").hashCode());
+    }
+}
