02d727bf0d5fe914064df1e8fa44295b18903c82
Kacper Świercz <224443@edu.p.lodz.pl>
2021-06-11 19:45:55 +0200

    Connect bookings list to backend

--- spa/src/app/services/mop/booking-general.service.ts
+++ spa/src/app/services/mop/booking-general.service.ts
@@ -1,83 +1,49 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { BookingGeneral } from '../../model/mop/BookingGeneral';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
-export class BookingGeneralService {
+export class BookingGeneralService implements OnDestroy {
 
     public bookings: BookingGeneral[];
+    private readonly url: string;
 
-    constructor() {
-        this.bookings = this.getEmptyBookings();
+    constructor(private http: HttpClient) {
+        this.url = environment.appUrl + '/bookings';
+        this.bookings = [];
     }
 
-    getEmptyBookings(): BookingGeneral[] {
-        return [
-            {
-                cruise: {
-                    startDate: new Date(),
-                    endDate: new Date(),
-                    route: {
-                        start: {
-                            city: 'City1',
-                            code: 'ABC'
-                        },
-                        destination: {
-                            city: 'City2',
-                            code: 'DEF'
-                        },
-                        code: 'ABCDEF'
-                    },
-                    ferry: {
-                        name: 'Ferry 1',
-                        onDeckCapacity: 100,
-                        vehicleCapacity: 1234
-                    },
-                    number: 'ABCDEF000001'
-                },
-                account: {
-                    login: 'string',
-                    active: true,
-                    firstName: 'Adam',
-                    lastName: 'Adam',
-                    accessLevel: [],
-                },
-                number: 'ABC0001',
-                creationDate: new Date()
-            },
-            {
-                cruise: {
-                    startDate: new Date(),
-                    endDate: new Date(),
-                    route: {
-                        start: {
-                            city: 'City1',
-                            code: 'ABC'
-                        },
-                        destination: {
-                            city: 'City2',
-                            code: 'DEF'
-                        },
-                        code: 'ABCDEF'
-                    },
-                    ferry: {
-                        name: 'Ferry 1',
-                        onDeckCapacity: 100,
-                        vehicleCapacity: 1234
-                    },
-                    number: 'ABCDEF000001'
-                },
-                account: {
-                    login: 'string',
-                    active: true,
-                    firstName: 'Adam',
-                    lastName: 'Adam',
-                    accessLevel: [],
-                },
-                number: 'ABC0002',
-                creationDate: new Date()
-            }
-        ];
+    private static parseDates(bookings: BookingGeneral[]): BookingGeneral[] {
+        bookings.forEach(booking => booking.creationDate = this.parseDate(booking.creationDate));
+        bookings.forEach(booking => booking.cruise.startDate = this.parseDate(booking.cruise.startDate));
+        bookings.forEach(booking => booking.cruise.endDate = this.parseDate(booking.cruise.endDate));
+        return bookings;
+    }
+
+    private static parseDate(stringDate: any): any {
+        if (!stringDate) {
+            return undefined;
+        }
+        return new Date(stringDate.toString().split('[UTC]')[ 0 ]);
+    }
+
+    getBookings(): Observable<BookingGeneral[]> {
+        return this.http.get<BookingGeneral[]>(this.url, {
+            observe: 'body',
+            responseType: 'json'
+        });
+    }
+
+    readBookings(bookings: BookingGeneral[]): void {
+        this.bookings = bookings;
+        this.bookings = BookingGeneralService.parseDates(this.bookings);
+    }
+
+    ngOnDestroy(): void {
+        this.bookings = [];
     }
 }
