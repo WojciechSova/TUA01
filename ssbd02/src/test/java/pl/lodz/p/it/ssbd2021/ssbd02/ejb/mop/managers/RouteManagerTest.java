88e755122eca21c54f840b2a6010a0f27f49cceb
Kacper Świercz <224443@edu.p.lodz.pl>
2021-06-13 10:02:42 +0200

    Add manager method for adding routes

--- ssbd02/src/test/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mop/managers/RouteManagerTest.java
+++ ssbd02/src/test/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mop/managers/RouteManagerTest.java
@@ -7,19 +7,26 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
+import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mop.facades.AccountMopFacade;
 import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mop.facades.interfaces.CruiseFacadeLocal;
 import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mop.facades.interfaces.RouteFacadeLocal;
+import pl.lodz.p.it.ssbd2021.ssbd02.ejb.mop.facades.interfaces.SeaportFacadeLocal;
+import pl.lodz.p.it.ssbd2021.ssbd02.entities.mok.Account;
 import pl.lodz.p.it.ssbd2021.ssbd02.entities.mop.Cruise;
 import pl.lodz.p.it.ssbd2021.ssbd02.entities.mop.Route;
+import pl.lodz.p.it.ssbd2021.ssbd02.entities.mop.Seaport;
 import pl.lodz.p.it.ssbd2021.ssbd02.exceptions.CommonExceptions;
 import pl.lodz.p.it.ssbd2021.ssbd02.exceptions.GeneralException;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class RouteManagerTest {
@@ -28,6 +35,10 @@ class RouteManagerTest {
     RouteFacadeLocal routeFacadeLocal;
     @Mock
     CruiseFacadeLocal cruiseFacadeLocal;
+    @Mock
+    AccountMopFacade accountMopFacade;
+    @Mock
+    SeaportFacadeLocal seaportFacadeLocal;
     @InjectMocks
     RouteManager routeManager;
 
@@ -35,6 +46,7 @@ class RouteManagerTest {
     Route route1;
     @Spy
     Route route2;
+
 
     @Spy
     Cruise cruise1;
@@ -75,5 +87,37 @@ class RouteManagerTest {
         when(cruiseFacadeLocal.findAllByRoute(route1)).thenReturn(Arrays.asList(cruise1, cruise2));
 
         assertEquals(Pair.of(route1, Arrays.asList(cruise1, cruise2)), routeManager.getRouteAndCruisesByRouteCode(code));
+    }
+
+    @Test
+    void createRoute() {
+        Seaport start = new Seaport();
+        Seaport destination = new Seaport();
+        start.setCode("ABC");
+        start.setCity("Abc");
+        destination.setCode("DEF");
+        destination.setCity("Def");
+        Account account = new Account();
+        account.setLogin("login");
+        when(seaportFacadeLocal.findByCode("ABC")).thenReturn(start);
+        when(seaportFacadeLocal.findByCode("DEF")).thenReturn(destination);
+        when(accountMopFacade.findByLogin("login")).thenReturn(account);
+
+        Route route = new Route();
+        route.setVersion(20L);
+        route.setCreationDate(Timestamp.from(Instant.now()));
+        route.setCode("ABCABC");
+
+        Timestamp before = Timestamp.from(Instant.now().minus(1L, ChronoUnit.MILLIS));
+        routeManager.createRoute(route, "ABC", "DEF", "login");
+        Timestamp after = Timestamp.from(Instant.now().plus(1L, ChronoUnit.MILLIS));
+
+        assertEquals(start, route.getStart());
+        assertEquals(destination, route.getDestination());
+        assertEquals(account, route.getCreatedBy());
+        assertTrue(route.getCreationDate().after(before) && route.getCreationDate().before(after));
+        assertEquals(0L, route.getVersion());
+
+        verify(routeFacadeLocal).create(route);
     }
 }
