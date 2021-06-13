3ebb5d40a33a3291cebfa17c9ba4eba0af3bdd28
Daniel Łondka <224360@edu.p.lodz.pl>
2021-06-13 14:25:03 +0200

    Update javadocs

--- ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mop/managers/interfaces/SeaportManagerLocal.java
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/ejb/mop/managers/interfaces/SeaportManagerLocal.java
@@ -49,6 +49,7 @@ public interface SeaportManagerLocal {
      * Metoda usuwa port o kodzie zawartym w encji {@link Seaport}.
      *
      * @param seaportCode Encja typu {@link Seaport}
+     * @param userLogin Login użytkownika usuwającego port
      */
     void removeSeaport(String seaportCode, String userLogin);
 }
--- ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/web/mop/SeaportEndpoint.java
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2021/ssbd02/web/mop/SeaportEndpoint.java
@@ -152,6 +152,13 @@ public class SeaportEndpoint {
         }
     }
 
+    /**
+     * Metoda umożliwiająca usunięcie portu.
+     *
+     * @param code kod identyfikujący port który chcemy usunąć
+     * @param securityContext Interfejs wstrzykiwany w celu pozyskania tożsamości aktualnie uwierzytelnionego użytkownika
+     * @return Kod 200 w przypadku udanego usunięcia portu
+     */
     @DELETE
     @Path("remove/{code}")
     @RolesAllowed({"EMPLOYEE"})
