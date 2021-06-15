3a149e9c562544fb2f0a460ed89f46142e9d16b3
Daniel Łondka <224360@edu.p.lodz.pl>
2021-06-15 21:04:44 +0200

    Connect updating seaport endpoint with spa, add messages based on responses

--- spa/src/app/ferrytales/seaport-details/seaport-edit/seaport-edit.component.ts
+++ spa/src/app/ferrytales/seaport-details/seaport-edit/seaport-edit.component.ts
@@ -8,22 +8,54 @@ import { SeaportDetailsService } from '../../../services/mop/seaport-details.ser
 })
 export class SeaportEditComponent {
 
+    readonly HIDDEN = 'hide';
+    readonly SUCCESS = 'success';
+    readonly GONE = 'gone';
+    readonly FAILURE = 'failure';
+    readonly OPTIMISTIC_LOCK = 'optimisticLock';
+    readonly NAME_NOT_UNIQUE = 'nameNotUnique';
+
     @Output()
     isEditSeaportFormVisible = new EventEmitter<any>();
 
     constructor(private seaportDetailsService: SeaportDetailsService) {
     }
 
-    changeSeaport(): void {
-        // TODO
+    changeSeaport(city: string): void {
+        this.seaportDetailsService.seaport.city = city;
+        this.seaportDetailsService.updateSeaport(this.seaportDetailsService.seaport)
+            .subscribe(() => {
+                    this.emit(this.SUCCESS);
+                },
+                (error) => {
+                    this.handleError(error);
+                });
     }
 
     closeComponent(): void {
+        this.emit(this.HIDDEN);
+    }
+
+    private emit(response: string): void {
         this.isEditSeaportFormVisible.emit({
-            seaportEdit: {
-                isFormVisible: true,
-                response: 'hide'
-            }
+                isFormVisible: false,
+                response
         });
+    }
+
+    private handleError(error: any): void {
+        if (error.status === 410) {
+            this.emit(this.GONE);
+        } else if (error.status === 409) {
+            if (error.error === 'ERROR.OPTIMISTIC_LOCK') {
+                this.emit(this.OPTIMISTIC_LOCK);
+            } else if (error.error === 'ERROR.SEAPORT_CITY_UNIQUE') {
+                this.emit(this.NAME_NOT_UNIQUE);
+            } else {
+                this.emit(this.FAILURE);
+            }
+        } else {
+            this.emit(this.FAILURE);
+        }
     }
 }
