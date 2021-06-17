1245223657af5cb1580143f21e42c1e1365a777f
224333 Kolanek Patryk <224333@edu.p.lodz.pl>
2021-06-17 16:02:24 +0200

    Add ferries list

--- spa/src/app/ferrytales/add-cruise/add-cruise.component.ts
+++ spa/src/app/ferrytales/add-cruise/add-cruise.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FerryGeneralService } from '../../services/mop/ferry-general.service';
+import { FerryGeneral } from '../../model/mop/FerryGeneral';
 
 @Component({
     selector: 'app-add-cruise',
@@ -10,19 +12,31 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AddCruiseComponent implements OnInit {
 
     code = '';
+    ferry = '';
 
     form = new FormGroup({
         number: new FormControl('', [Validators.required, Validators.pattern('[A-Z]{6}[0-9]{6}')]),
         startDate: new FormControl('', Validators.required),
-        endDate: new FormControl('', Validators.required)
+        endDate: new FormControl('', Validators.required),
+        ferry: new FormControl('', Validators.required)
     });
 
     constructor(private route: ActivatedRoute,
-                private router: Router) {
+                private router: Router,
+                public ferryGeneralService: FerryGeneralService) {
         this.code = this.route.snapshot.paramMap.get('code') as string;
+        this.getFerries();
     }
 
     ngOnInit(): void {
+    }
+
+    getFerries(): void {
+        this.ferryGeneralService.getFerries().subscribe(
+            (response: FerryGeneral[]) => {
+                this.ferryGeneralService.ferriesGeneralList = response;
+            }
+        );
     }
 
     goToHomeBreadcrumb(): void {
