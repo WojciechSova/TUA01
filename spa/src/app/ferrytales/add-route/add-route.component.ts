5531674056de60f090cf28f4a3ca2ff361acbc55
Kacper Świercz <224443@edu.p.lodz.pl>
2021-06-18 17:53:29 +0200

    Fix selects

--- spa/src/app/ferrytales/add-route/add-route.component.ts
+++ spa/src/app/ferrytales/add-route/add-route.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {SeaportGeneralService} from "../../services/mop/seaport-general.service";
-import {SeaportDetails} from "../../model/mop/SeaportDetails";
-import {SeaportGeneral} from "../../model/mop/SeaportGeneral";
+import { FormControl , FormGroup, Validators } from '@angular/forms';
+import { SeaportGeneralService } from '../../services/mop/seaport-general.service';
+import { SeaportGeneral } from '../../model/mop/SeaportGeneral';
 
 @Component({
     selector: 'app-add-route',
@@ -14,10 +13,11 @@ export class AddRouteComponent implements OnInit {
 
     form = new FormGroup({
         code: new FormControl('', [Validators.required, Validators.pattern('[A-Z]{6}')]),
+        start: new FormControl('', [Validators.required]),
+        dest: new FormControl('', [Validators.required]),
     });
 
     start = '';
-
     dest = '';
 
     constructor(private router: Router,
@@ -39,16 +39,16 @@ export class AddRouteComponent implements OnInit {
     }
 
     addRoute(value: string): void {
-        console.log(this.start);
-        console.log(this.seaportGeneralService.seaportsList);
+        return;
     }
 
-    setStartSeaport(seaport: string): void {
-        this.start = seaport;
-        console.log(this.start);
+    getSeaports(): SeaportGeneral[] {
+        return this.seaportGeneralService.seaportsList.filter(
+            seaport => seaport.code !== this.start && seaport.code !== this.dest
+        );
     }
 
-    setDestSeaport(seaport: string): void {
-        this.dest = seaport;
+    getCity(code: string): string | undefined {
+        return this.seaportGeneralService.seaportsList.find(seaport => seaport.code === code)?.city;
     }
 }
