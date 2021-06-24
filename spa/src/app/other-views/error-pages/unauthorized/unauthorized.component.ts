9d1856b9295a398bd8212e8e8b0928ca877ecfb5
Wojciech Sowa <224429@edu.p.lodz.pl>
2021-06-24 13:55:27 +0200

    Move code to app component

--- spa/src/app/other-views/error-pages/unauthorized/unauthorized.component.ts
+++ spa/src/app/other-views/error-pages/unauthorized/unauthorized.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
-import { AuthService } from '../../../services/mok/auth.service';
 
 @Component({
     selector: 'app-unauthorized',
@@ -10,15 +8,7 @@ import { AuthService } from '../../../services/mok/auth.service';
 })
 export class UnauthorizedComponent implements OnInit {
 
-    constructor(private router: Router,
-                private cookieService: CookieService,
-                private authService: AuthService) {
-        const expirationTime = cookieService.get('expirationTime');
-        if (expirationTime != null && expirationTime !== '' && expirationTime < (Date.now().valueOf() / 1000).toString()) {
-            this.authService.signOut();
-            this.router.navigate(['/']);
-        }
-    }
+    constructor(private router: Router) {}
 
     goToHomePage(): void {
         this.router.navigate(['/']);
