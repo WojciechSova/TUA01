ba84cedb5cbdaa17ce0ac00a14cade164bc12a9b
Wojciech Sowa <224429@edu.p.lodz.pl>
2021-06-24 12:05:56 +0200

    Delete cookies and navigate to home page when user have expired token in cookies

--- spa/src/app/other-views/error-pages/unauthorized/unauthorized.component.ts
+++ spa/src/app/other-views/error-pages/unauthorized/unauthorized.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../../../services/mok/auth.service';
 
 @Component({
     selector: 'app-unauthorized',
@@ -8,7 +10,14 @@ import { Router } from '@angular/router';
 })
 export class UnauthorizedComponent implements OnInit {
 
-    constructor(private router: Router) {
+    constructor(private router: Router,
+                private cookieService: CookieService,
+                private authService: AuthService) {
+        const expirationTime = cookieService.get('expirationTime');
+        if (expirationTime != null && expirationTime !== '' && expirationTime < (Date.now().valueOf() / 1000).toString()) {
+            this.authService.signOut();
+            this.router.navigate(['/']);
+        }
     }
 
     goToHomePage(): void {
